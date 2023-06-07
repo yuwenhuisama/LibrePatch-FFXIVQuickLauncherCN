00ad9c9c3618320dc4a81cb24f23fb233def05a7
Huisama <yuwenhuisama@outlook.com>
2023-06-07 20:57:30 +0800

    [feat] add config property

--- Consts.cs
+++ Consts.cs
@@ -25,21 +25,27 @@ public class Config
 
     [JsonPropertyName("XIVLauncherBasePath")]
     public string XIVLauncherBasePath { get; set; }
-    
+
     [JsonPropertyName("DalamudAssemblyName")]
     public string DalamudAssemblyName { get; set; }
-    
+
     [JsonPropertyName("DalamudBaseDirectoryToScan")]
     public string DalamudBaseDirectoryToScan { get; set; }
-    
+
     [JsonPropertyName("PluginManagerName")]
     public string PluginManagerName { get; set; }
-    
+
     [JsonPropertyName("HackStrategyVersion")]
     public int HackStrategyVersion { get; set; }
-    
+
     [JsonPropertyName("AssetStoreUrlFieldName")]
     public string AssetStoreUrlFieldName { get; set; }
+
+    [JsonPropertyName("LocalFileSystemBasePath")]
+    public string LocalFileSystemBasePath { get; set; }
+
+    [JsonPropertyName("LocalCheatFileName")]
+    public string LocalCheatFileName { get; set; }
 }
 
 public static class Consts
@@ -49,8 +55,17 @@ public static class Consts
     public static string MethodNameToRewrite = "CheckAssetRefreshNeeded";
     public static string MetaUrl = "https://aonyx.ffxiv.wang/Dalamud/Asset/Meta";
 
+    public static string LocalFileSystemBaseUrl = "/file";
+    public static string LocalFileSystemBasePath = "./file";
+
+    public static string LocalCheatFileName =
+        "cheatplugin.011262350f30f39ad448bd802e653a7067c10c072f42cd973c2302c65a5e2661.json";
+
+    public static string LocalCheatFilePath = Path.Combine(LocalFileSystemBasePath, LocalCheatFileName);
+
     public static string LocalFakeCheatFileUrl =
-        "http://127.0.0.1:9999/file/cheatplugin.011262350f30f39ad448bd802e653a7067c10c072f42cd973c2302c65a5e2661.json";
+        $"{MockBaseUrl}:{MockPort}{LocalFileSystemBaseUrl}/{LocalCheatFileName}";
+
     public static string MockBaseUrl = "http://127.0.0.1";
     public static int MockPort = 9999;
     public static string XIVLauncherBasePath = "./";
@@ -78,6 +93,11 @@ public static class Consts
             MockPort = confObj.MockPort;
             XIVLauncherBasePath = confObj.XIVLauncherBasePath;
             AssetStoreUrlFieldName = confObj.AssetStoreUrlFieldName;
+
+            LocalFileSystemBasePath = confObj.LocalFileSystemBasePath;
+            LocalCheatFileName = confObj.LocalCheatFileName;
+            LocalCheatFilePath = Path.Combine(LocalFileSystemBasePath, LocalCheatFileName);
+            LocalFakeCheatFileUrl = $"{MockBaseUrl}:{MockPort}/file/{LocalCheatFileName}";
 
             DalamudAssemblyName = confObj.DalamudAssemblyName;
             DalamudBaseDirectoryToScan = confObj.DalamudBaseDirectoryToScan;
